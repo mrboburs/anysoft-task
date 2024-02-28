d16aa783542998e30792d864d93d35a008a0c98d
Bobur Abdusalomov <babdusalom72@gmail.com>
2024-02-28 11:31:58 +0500

    empity repo

--- dist/src/shared/decorators/id-validation.decorator.d.ts
+++
@@ -1,4 +0,0 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
-export declare class IdValidationPipe implements PipeTransform<any> {
-    transform(value: any, metadata: ArgumentMetadata): Promise<any>;
-}
